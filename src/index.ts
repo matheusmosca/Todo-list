45b9d9948f5b25cf61ba9d031ec8b9ef2802a101
matheusmosca <matheusmoscag@gmail.com>
2020-03-16 16:16:03 -0300

    Adding the layout

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-// import {Promise} from 'es6-promise'
-import { Observable } from 'rxjs'
-
-const basic: any = Observable.create((observer:any) => {
-    observer.next("oi");
-})
-
-basic.subscribe((x:any) => {
-    console.log(x);
-})
-
-console.log("Test")
-
+console.log("hello")
+console.log("hello")
